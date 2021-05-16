fa08b83749bb75cb6ab72b1db7f801cdabd6c079
DESKTOP-NTLGR3I\ingeu <ingeun92@naver.com>
2021-05-17 00:59:28 +0900

    Update - ServeStaticModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { UsersModule } from "./users/users.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb://localhost/nestjs"), UsersModule],
+  imports: [
+    MongooseModule.forRoot("mongodb://localhost/nestjs"),
+    UsersModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "public"),
+      exclude: ["/*"],
+    }),
+  ],
 })
 export class AppModule {}
